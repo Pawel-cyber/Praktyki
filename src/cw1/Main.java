f2b99785467349dbc84d6b11baa6fcaabfd5e3fb
Paweł <74782912+Pawel-cyber@users.noreply.github.com>
2020-11-27 20:06:43 +0100

    Update Main.java

--- src/cw1/Main.java
+++ src/cw1/Main.java
@@ -2,6 +2,13 @@ package cw1;
 
 public class Main {
     public static void main(String[] args) {
+        
+    for (int i = 0; i <= 10; i++) {
+
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
 
 
 

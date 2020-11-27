c50902e9243347531dc936d6e7aa4362fbc1b0b8
Pawel-cyber <szymanskip524@gmail.com>
2020-11-27 14:00:47 +0100

    First

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+            int a = 0;
+            Random ran = new Random();
+            for  (int  i = 0; i < 10; i++) {
+
+                int c = ran.nextInt(1000) + 1;
+
+
+                a = a + c;
+                System.out.println("Losowa liczba ="+c);
+
+            }
+            System.out.println("suma Losowych liczb  ="+" "+" "+a);
+    }
+}
+
+

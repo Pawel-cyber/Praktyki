ea719ae2a965a85532abe61a031f9d47a071d408
Paweł <74782912+Pawel-cyber@users.noreply.github.com>
2020-12-01 19:45:54 +0100

    Update Main.java

--- src/cw3/Main.java
+++ src/cw3/Main.java
@@ -1,8 +1,17 @@
 package cw3;
+public class cw3 {
+    public static void main(String[] args){
 
-public class Main {
-    public static  void main(String[] args){
+        int[] tab = {2,2,3,4,5,6,2,8,9,10};
+        int[] wynik = new int[tab.length];
 
+        int a = 0;
+        for  (int  i = 0; i < 10; i++) {
 
+            a = a + tab[i];
+            System.out.println(i+"Liczba ="+tab[i]);
+
+        }
+        System.out.println("suma liczb  ="+" "+" "+a);
     }
 }
